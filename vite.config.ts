0b0e1de0fd27525fb5a84d89339674d0cfaa578c
kalpalathika <kalpalathika1@gmail.com>
2026-03-19 21:06:34 -0700

    fix router basename

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/SRR-Technical-Services/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +12,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [react()], // ✅ updated here
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
